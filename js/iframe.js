4f238b522604f5bbf4e437254e3ecf395c474e66
Choi Jung Yun <bebe1224@naver.com>
2025-03-07 18:50:54 +0900

    2025-03-07

--- js/iframe.js
+++ js/iframe.js
@@ -1,13 +1,13 @@
 
-const videoList = document.querySelectorAll('.video-list li');
-const iframe = document.getElementById('video-frame');
+// const videoList = document.querySelectorAll('.video-list li');
+// const iframe = document.getElementById('video-frame');
 
-videoList.forEach((item) => {
-    item.addEventListener('click', () => {
-        const videoUrl = item.getAttribute('data-video');
-        iframe.src = videoUrl; // 클릭 시에만 아이프레임 URL 설정
-    });
-});
+// videoList.forEach((item) => {
+//     item.addEventListener('click', () => {
+//         const videoUrl = item.getAttribute('data-video');
+//         iframe.src = videoUrl; // 클릭 시에만 아이프레임 URL 설정
+//     });
+// });
 // ===
 
 
